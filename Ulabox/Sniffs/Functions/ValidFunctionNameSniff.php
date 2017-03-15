e53b174814c281f6de8596d69df1d0f9ea713f73
Jesús Díez <jesus.diez@ulabox.com>
2017-03-15 18:25:44 +0100

    feat: Check if method name is required by parent class or interface

--- Ulabox/Sniffs/Functions/ValidFunctionNameSniff.php
+++ Ulabox/Sniffs/Functions/ValidFunctionNameSniff.php
@@ -54,6 +54,10 @@ class Ulabox_Sniffs_Functions_ValidFunctionNameSniff implements PHP_CodeSniffer_
                 return;
             }
 
+            if ($this->isNeededByParentClass($name, $tokens)) {
+                return;
+            }
+
             if (substr($name, 0, 3) == 'set' && ctype_upper(substr($name, 3, 1))) {
                 $phpcsFile->addWarning(
                     sprintf('Public setter "%s" starts with "set". Write a name that express a domain behavior', $name),
@@ -70,5 +74,66 @@ class Ulabox_Sniffs_Functions_ValidFunctionNameSniff implements PHP_CodeSniffer_
                 );
             }
         }
+    }
+
+    private function isNeededByParentClass($methodName, array $tokens)
+    {
+        $classFQN = $this->resolveClassFQN($tokens);
+        $reflection = new ReflectionClass($classFQN);
+
+        return $this->isMethodDefinedInParent($methodName, $reflection) ||
+            $this->isMethodDefinedInInterface($methodName, $reflection);
+    }
+
+    private function isMethodDefinedInInterface($methodName, $reflection)
+    {
+        $interfaces = $reflection->getInterfaces();
+        if (empty($interfaces)) {
+            return false;
+        }
+        foreach ($interfaces as $interface) {
+            if ($interface->hasMethod($methodName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isMethodDefinedInParent($methodName, $reflection)
+    {
+        $parent = $reflection->getParentClass();
+        if (!$parent) {
+            return $reflection->hasMethod($methodName);
+        } else {
+            return $this->isMethodDefinedInParent($methodName, $parent->getNamespaceName());
+        }
+    }
+
+    private function resolveClassFQN(array $tokens)
+    {
+        $namespace = '';
+        $class = '';
+        $numTokens = count($tokens);
+        for ($i = 0; $i < $numTokens; $i++) {
+            if ($tokens[$i]['type'] === 'T_NAMESPACE') {
+                for ($j = $i + 1; $j < $numTokens; $j++) {
+                    if ($tokens[$j]['type'] === 'T_STRING') {
+                        $namespace .= '\\'.$tokens[$j]['content'];
+                    } else if ($tokens[$j]['content'] === '{' || $tokens[$j]['content'] === ';') {
+                        break;
+                    }
+                }
+            }
+            if ($tokens[$i]['type'] === 'T_CLASS') {
+                for ($j=$i+1;$j<$numTokens;$j++) {
+                    if ($tokens[$j]['content'] === '{') {
+                        $class = $tokens[$i+2]['content'];
+                    }
+                }
+            }
+        }
+
+        return $namespace.'\\'.$class;
     }
 }
